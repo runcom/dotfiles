415eb7b751df2e5ee34a7e361d152200457b8c17
Antonio Murdaca <runcom@redhat.com>
2016-11-14 10:38:52 +0100

    wayland ftw
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- kem.sh
+++ kem.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xinput set-button-map "Kensington      Kensington Expert Mouse" 1 1 3 4 5 6 7 2
+id=$(xinput list | grep xwayland-pointer | awk '{{ print $4  }}' | cut -d"=" -f2)
+xinput set-button-map $id 1 1 3 4 5 6 7 2
