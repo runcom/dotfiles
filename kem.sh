a61878c2468f092cf229d4506c353f91157347a7
Antonio Murdaca <runcom@redhat.com>
2017-05-17 12:20:18 +0200

    update kem.sh
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- kem.sh
+++ kem.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 id=$(xinput list | grep xwayland-pointer | awk '{{ print $4  }}' | cut -d"=" -f2)
-xinput set-button-map $id 1 1 3 4 5 6 7 2
+xinput set-button-map $id 1 1 3 4 5 6 7 2 2&>/dev/null|| true
+id2=$(xinput list | grep Kensi | awk '{{print $7}}' | cut -d"=" -f2)
+xinput set-button-map $id2 1 1 3 4 5 6 7 2
